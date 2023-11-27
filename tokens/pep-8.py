dfb46ebc940a2700dc1616e071ce583514c08750
Brijesh Gondaliya <140867807+brijeshpytops@users.noreply.github.com>
2023-11-27 14:30:41 +0530

    Create pep-8.py

---
+++ tokens/pep-8.py
@@ -0,0 +1,4 @@
+"""
+
+PEP 8 is a style guide for writing Python code. It stands for "Python Enhancement Proposal 8" and was authored by Guido van Rossum, Python's creator, and Barry Warsaw. PEP 8 provides guidelines and recommendations on how to format your Python code to make it more readable and consistent. Adhering to PEP 8 helps improve the overall quality of Python code and makes it easier for developers to collaborate on projects.
+"""
