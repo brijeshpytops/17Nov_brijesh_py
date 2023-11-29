6718b1a00c94adc09f8b249645ad5e63a27dfa9a
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2023-11-29 15:20:15 +0530

    datatypes and operators

---
+++ operators/main.py
@@ -0,0 +1,40 @@
+"""
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+"""
+# Arithmetic operators
+a = 10
+b = 20
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a * b)
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+# x = 33
+# y = 3
+# print(x / y)
+
+# Assignment operators
+
+# a = 10
+
+# # a = a + 10
+# a += 10
+# a -= 10
+# a *= 10
+# a /= 10
+# a %= 10
+# a **= 10
+# a //= 10
+# print(a)
